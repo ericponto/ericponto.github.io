ddcd409a2e790c4cdfaa295f5dd2d9fffc6031e4
Eric Ponto <eric.ponto@gmail.com>
2015-02-04 00:24:58 +0000

    a few more touches to the redesign

--- _build/contrib-copy.js
+++ _build/contrib-copy.js
@@ -3,6 +3,10 @@ module.exports = function(grunt) {
 		css: {
 			src: "css/styles.min.css",
 			dest: "_includes/styles.html"
+		},
+		html5shiv: {
+			src: "_lib/html5shiv/dist/html5shiv.min.js",
+			dest: "js/html5shiv.min.js"
 		}
 	});
 
